ff3dae0386841b4c6698b599f3242599526beb07
Irfan Halim <irfanhalimhidayat@gmail.com>
2023-02-27 14:12:11 +0700

    adding custom error

--- app/api/v1/categories/controller.js
+++ app/api/v1/categories/controller.js
@@ -41,7 +41,10 @@ const index = async (req, res, next) => {
 const find = async (req, res, next) => {
     try {
         const { id } = req.params;
-        const result = await Categories.findOne({ id });
+
+        const result = await Categories.findOne(
+            { _id: id },
+        );
         if (!result) {
             return res.status(404).json({ message: 'Id categories tidak ditemukan' });
         }
@@ -53,8 +56,52 @@ const find = async (req, res, next) => {
     }
 };
 
+const update = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const { name } = req.body;
+
+        // const result = await Categories.findOneAndUpdate(
+        //     { _id: id },
+        //     { name }
+        // );
+        // if (!result) {
+        //     return res.status(404).json({ message: 'Id categories tidak ditemukan' });
+        // }
+
+        // result.name = name;
+        // result.save();
+
+        const result = await Categories.findOneAndUpdate(
+            { _id: id },
+            { name },
+            { new: true }
+        );
+
+        res.status(200).json({
+            data: result,
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+const remove = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const result = await Categories.findByIdAndRemove(id);
+        res.status(200).json({
+            data: result
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
 module.exports = {
     index,
     find,
     create,
+    update,
+    remove
 };
--- app/api/v1/categories/router.js
+++ app/api/v1/categories/router.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express();
-const { create, index, find } = require('./controller');
+const { create, index, find, update, remove } = require('./controller');
 
 router.get('/categories', index);
 
@@ -8,22 +8,26 @@ router.get('/categories/:id', find);
 
 router.post('/categories', create);
 
-router.get('/categories/:id', (req, res) => {
-    res.status(203).json({
-        message: 'Categorie by Id'
-    });
-});
+router.put('/categories/:id', update);
 
-router.put('/categories/:id', (req, res) => {
-    res.status(204).json({
-        message: 'success update categorie by id'
-    });
-});
+router.delete('/categories/:id', remove);
 
-router.delete('/categories/:id', (req, res) => {
-    res.status(205).json({
-        message: 'Catogories Deleted'
-    });
-});
+// router.get('/categories/:id', (req, res) => {
+//     res.status(203).json({
+//         message: 'Categorie by Id'
+//     });
+// });
+
+// router.put('/categories/:id', (req, res) => {
+//     res.status(204).json({
+//         message: 'success update categorie by id'
+//     });
+// });
+
+// router.delete('/categories/:id', (req, res) => {
+//     res.status(205).json({
+//         message: 'Catogories Deleted'
+//     });
+// });
 
 module.exports = router;
