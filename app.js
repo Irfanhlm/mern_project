c7f86b3a67a9a32c5c1957ac47241425adf9f5a2
Irfan Halim <irfanhalimhidayat@gmail.com>
2023-03-01 09:31:06 +0700

    Custom Error

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ const imagesRouter = require('./app/api/v1/images/router');
 const ordersRouter = require('./app/api/v1/orders/router');
 const ticketsCategoriesRouter = require('./app/api/v1/tickets_categories/router');
 
+const notFoundMiddleware = require('./app/middlewares/not-found');
+const handleErrorMiddleware = require('./app/middlewares/handler-eror');
+
 const v1 = '/api/v1/cms';
 const v2 = '/api/v1';
 
@@ -21,17 +24,21 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'Welcome to api semina',
+    });
+});
+
 app.use(v1, categoriesRouter);
 app.use(v1, talentsRouter);
 app.use(v1, imagesRouter);
 app.use(v2, ordersRouter);
 app.use(v1, ticketsCategoriesRouter);
 
+app.use(notFoundMiddleware);
+app.use(handleErrorMiddleware);
+
 module.exports = app;
 
 
-app.get('/', (req, res) => {
-    res.status(200).json({
-        message: 'Welcome to api semina',
-    });
-});
--- app/api/v1/categories/router.js
+++ app/api/v1/categories/router.js
@@ -1,33 +1,11 @@
+const { create, index, find, update, remove } = require('./controller');
 const express = require('express');
 const router = express();
-const { create, index, find, update, remove } = require('./controller');
 
 router.get('/categories', index);
-
 router.get('/categories/:id', find);
-
 router.post('/categories', create);
-
 router.put('/categories/:id', update);
-
 router.delete('/categories/:id', remove);
-
-// router.get('/categories/:id', (req, res) => {
-//     res.status(203).json({
-//         message: 'Categorie by Id'
-//     });
-// });
-
-// router.put('/categories/:id', (req, res) => {
-//     res.status(204).json({
-//         message: 'success update categorie by id'
-//     });
-// });
-
-// router.delete('/categories/:id', (req, res) => {
-//     res.status(205).json({
-//         message: 'Catogories Deleted'
-//     });
-// });
 
 module.exports = router;
